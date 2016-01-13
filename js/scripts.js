ce0a547125656b963a3b7be3269c5c1e44dbd72e
Scott Holland <scotthollandjr@gmail.com>
2016-01-12 20:33:06 -0800

    Add user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 var leapYear = function(year) {
   return ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0));
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+    if (!result) {
+      $(".not").text("not");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
